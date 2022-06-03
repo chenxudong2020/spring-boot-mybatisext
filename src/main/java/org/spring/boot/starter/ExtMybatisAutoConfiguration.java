21c63632033fd825769c3b5514c85bc3d1d61aef
chenxudong2020 <872603935@qq.com>
2022-06-03 20:18:28 +0800

    Initial commit

---
+++ src/main/java/org/spring/boot/starter/ExtMybatisAutoConfiguration.java
@@ -0,0 +1,105 @@
+package org.spring.boot.starter;
+
+import com.ext.mybatisext.MapperRegistryExt;
+import com.ext.mybatisext.SqlSessionFactoryBeanExt;
+import com.ext.mybatisext.helper.ArrayTypeHandlerExt;
+import com.ext.mybatisext.helper.SmartDate;
+import com.ext.mybatisext.helper.SmartDateTypeHandler;
+import com.ext.mybatisext.helper.SqlSessionFactoryHolder;
+import com.ext.mybatisext.interceptor.MyBatisInterceptor;
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.mapping.DatabaseIdProvider;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+
+import javax.sql.DataSource;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.List;
+/**
+ * @author 87260
+ */
+@SuppressWarnings("all")
+@Configuration
+@ConditionalOnClass({SqlSessionFactory.class, SqlSessionFactoryBeanExt.class,SqlSessionFactoryBean.class})
+@ConditionalOnBean({DataSource.class})
+@EnableConfigurationProperties({MybatisProperties.class})
+@AutoConfigureAfter({DataSourceAutoConfiguration.class})
+public class ExtMybatisAutoConfiguration extends MybatisAutoConfiguration {
+    private static final Log logger = LogFactory.getLog(ExtMybatisAutoConfiguration.class);
+
+    private  MybatisProperties properties;
+    private  Interceptor[] interceptors;
+    private  ResourceLoader resourceLoader;
+    private  DatabaseIdProvider databaseIdProvider;
+    private  List<ConfigurationCustomizer> configurationCustomizers;
+
+    @Autowired
+     private MyBatisInterceptor[] myBatisInterceptors;
+
+
+    public ExtMybatisAutoConfiguration(MybatisProperties properties, ObjectProvider<Interceptor[]> interceptorsProvider, ResourceLoader resourceLoader, ObjectProvider<DatabaseIdProvider> databaseIdProvider, ObjectProvider<List<ConfigurationCustomizer>> configurationCustomizersProvider) {
+        super(properties, interceptorsProvider, resourceLoader, databaseIdProvider, configurationCustomizersProvider);
+    }
+
+
+
+    private void setValue( String name, Object value ) {
+        try {
+            Field field = SqlSessionFactoryBean.class.getDeclaredField(name);
+            field.setAccessible(true);
+            field.set(this, value);
+        } catch ( Exception e ) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+
+    @Override
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactory factory=super.sqlSessionFactory(dataSource);
+        if (factory == null ) {
+            factory = super.sqlSessionFactory(dataSource);
+            setValue("sqlSessionFactory", factory);
+            SqlSessionFactoryHolder.setSqlSessionFactory(factory);
+        }
+        try {
+            org.apache.ibatis.session.Configuration config = factory.getConfiguration();
+            Class<?> classConfig = org.apache.ibatis.session.Configuration.class;
+            // 拦截
+            Field field = classConfig.getDeclaredField("mapperRegistry");
+            field.setAccessible(true);
+            field.set(config, new MapperRegistryExt(config, myBatisInterceptors));
+            // 日期格式处理
+            config.getTypeHandlerRegistry().register(java.util.Date.class, SmartDateTypeHandler.class);
+            config.getTypeHandlerRegistry().register(java.sql.Date.class, SmartDateTypeHandler.class);
+            config.getTypeHandlerRegistry().register(SmartDate.class, SmartDateTypeHandler.class);
+            config.getTypeHandlerRegistry().register(String[].class, ArrayTypeHandlerExt.class);
+            config.getTypeHandlerRegistry().register(Integer[].class, ArrayTypeHandlerExt.class);
+            config.getTypeHandlerRegistry().register(BigDecimal[].class, ArrayTypeHandlerExt.class);
+            config.getTypeHandlerRegistry().register(Double[].class, ArrayTypeHandlerExt.class);
+            config.getTypeHandlerRegistry().register(Float[].class, ArrayTypeHandlerExt.class);
+            config.getTypeHandlerRegistry().register(Long[].class, ArrayTypeHandlerExt.class);
+
+        } catch ( Exception e ) {
+            logger.error(e.getMessage(), e);
+        }
+        return factory;
+    }
+
+
+}
---
+++ src/main/java/org/spring/boot/starter/MybatisConfiguration.java
@@ -0,0 +1,69 @@
+package org.spring.boot.starter;
+
+import com.ext.mybatisext.ExtObjectWrapperFactory;
+import com.ext.mybatisext.activerecord.spring.SpringSupportBean;
+import com.ext.mybatisext.interceptor.CommonMapperInterceptor;
+import com.ext.mybatisext.interceptor.GenericMapperInterceptor;
+import com.ext.mybatisext.interceptor.MyBatisInterceptor;
+import com.ext.mybatisext.plugin.BatchPlugin;
+import com.ext.mybatisext.plugin.IdentityPlugin;
+import com.ext.mybatisext.plugin.IndexingPlugin;
+import com.ext.mybatisext.plugin.SQLPrintPlugin;
+import com.ext.mybatisext.plugin.paging.PagingPlugin;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author 87260
+ */
+@Configuration
+public class MybatisConfiguration {
+
+
+    @Autowired
+    DataSource dataSource;
+
+    /**
+     *  加载自定义插件,设置Map值的key为驼峰处理
+     * @return
+     */
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer(){
+        return configuration -> {
+            //TODO 加载自定义插件
+            configuration.addInterceptor(new SQLPrintPlugin());
+            configuration.addInterceptor(new IndexingPlugin());
+            configuration.addInterceptor(new PagingPlugin());
+            configuration.addInterceptor(new IdentityPlugin());
+            configuration.addInterceptor(new BatchPlugin());
+            //TODO 设置Map值的key为驼峰处理
+            configuration.setObjectWrapperFactory(new ExtObjectWrapperFactory());
+        };
+    }
+
+    /**
+     * 加载定义拦截器处理
+     * @return
+     */
+    @Bean(name="myBatisInterceptor")
+    public MyBatisInterceptor[] getInterceptors(){
+        return new MyBatisInterceptor[]{
+                new CommonMapperInterceptor(),
+                new GenericMapperInterceptor()};
+    }
+
+
+
+
+    @Bean
+    public SpringSupportBean getSpringSupportBean(){
+        SpringSupportBean springSupportBean= new SpringSupportBean();
+        springSupportBean.setDataSource(dataSource);
+        return springSupportBean;
+    }
+
+}
